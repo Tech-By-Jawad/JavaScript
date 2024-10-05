a340d0b4b12ff21c81e1426f46ea97948469ce45
Jawad Hussain <techbyjawad@gmail.com>
2024-10-05 20:04:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // for using only nre javascript concepts.
+
+alert(3+3) // only used on browser as we are sing nodejs so it will not work.
+
+/* DataTypes
+
+Primitive
+1- number => 2 to power of 53
+2- bigInt => it only use on big websites
+3- string
+4- boolean => true/false
+5- Null => standalone value
+6- undefined
+7- Symbol => whenever we are talking about uniqueness then we will use symbol
+ 
+Non-Primitive
+1- object
+*/
